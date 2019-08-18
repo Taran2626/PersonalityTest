fad0c1125c03f4af67d4b8f73ca16eaa9e23acce
Taran <taran.codebrew@gmail.com>
2019-08-18 22:57:53 +0800

    Questionnaire Screens and Logic

---
+++ PersonalityTest/MVVM/View Model/QuestionViewModal.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionViewModal.swift
+//  PersonalityTest
+//
+//  Created by Saif Chaudhary on 18/08/19.
+//  Copyright © 2019 Taranjeet_MacBook. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewModal: NSObject {
+
+}
